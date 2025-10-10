63ef2131a27d29f9471c93d6ff52ea3e2166e2c3
Confusified <60018584+Confusified@users.noreply.github.com>
2025-10-10 14:36:45 +0200

    aanmaken en registreren van de classes, services en repositories

---
+++ Grocery.Core/Models/Category.cs
@@ -0,0 +1,17 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Grocery.Core.Models
+{
+    public class Category(int id, string name) : ObservableObject
+    {
+        public int Id { get; set; } = id;
+        [ObservableProperty]
+        public string name = name;
+
+    }
+}
---
+++ Grocery.Core/Models/ProductCategory.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Grocery.Core.Models
+{
+    public class ProductCategory : Category
+    {
+        public int ProductId { get; set; }
+        public int CategoryId { get; set; }
+
+        public ProductCategory(int id, string name) : base(id, name) { }
+
+        
+    }
+}
